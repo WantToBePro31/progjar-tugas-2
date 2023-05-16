a413e60bb06a6c67cdd055763510698d36e80a3d
WantToBePro31 <frederick.wijayadisusilo@gmail.com>
2023-05-17 05:44:00 +0700

    add Tugas 2 server & client

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import logging
+
+def send_data():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 45000)
+    logging.warning(f"opening socket {server_address}")
+    sock.connect(server_address)
+
+    try:
+        # Send data
+        msg = 'TIME\r\n'
+        logging.warning(f"sending {msg}")
+        sock.sendall(msg.encode('utf-8'))
+        # Look for the response
+        data = sock.recv(32)
+        result = data.decode('utf-8')
+        logging.warning(f"{result}")
+    finally:
+        logging.warning("closing")
+        sock.close()
+
+if __name__=='__main__':
+    send_data()
